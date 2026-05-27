eac3247f9ca812c39c8079d6015939a82caa556b
lsmerpthangesh-max <lsmerpthangesh@gmail.com>
2026-05-27 15:02:47 +0530

    Update shoplifting_detection.py

--- shoplifting_detection.py
+++ shoplifting_detection.py
@@ -14,7 +14,7 @@ input_path="res/inout1.mp4"
 output_path=""
 
 #model
-mymodel=YOLO("configs/shoplifting_weights.pt")
+mymodel=YOLO("configs/shoplifting_wights.pt")
 
 # input fils
 cap = cv2.VideoCapture(input_path)  
